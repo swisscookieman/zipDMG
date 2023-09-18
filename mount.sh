47e1b760fe2afae5f8e4c9fd2001bb0e03105690
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-09-18 18:37:37 +0200

    Citra

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://cclerc.live/db/lan.dmg"
+dmg_url="https://github.com/citra-emu/citra-web/releases/download/2.0/citra-setup-mac.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
