215f5a831f01c17b7608e2b41d2dd8a462862c2b
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-09-19 08:44:44 +0200

    gc

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://dl.discordapp.net/apps/osx/0.0.277/Discord.dmg"
+dmg_url="https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
