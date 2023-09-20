20f70a04fbc42bf1aa85a4f62a1e7df1549405de
autowebsite <cclerc@cclerc.live>
2023-09-20 10:19:46 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://cclerc.live/db/arc.dmg"
+dmg_url="https://cclerc.live/arc.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
