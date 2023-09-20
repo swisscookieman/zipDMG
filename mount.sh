4024d88152f1687b64cb0b27c12b831fdfbc5e78
autowebsite <cclerc@cclerc.live>
2023-09-20 18:48:03 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="15 abonnéhttps://cdn.akamai.steamstatic.com/client/installer/steam.dmg"
+dmg_url="https://cdn.akamai.steamstatic.com/client/installer/steam.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
