a18976694d1716c12bd48ea493be0d96a1acba96
autowebsite <cclerc@cclerc.live>
2023-09-21 18:46:14 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/cemu-project/Cemu/releases/download/v2.0-51/cemu-2.0-51-macos-12-x64.dmg"
+dmg_url="https://github.com/utmapp/UTM/releases/latest/download/UTM.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
