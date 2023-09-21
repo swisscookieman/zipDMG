e9bbbbb17110ab363b5e07ebfa6f50f0cfa56042
autowebsite <cclerc@cclerc.live>
2023-09-21 16:08:11 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://cloud.bluestacks.com/api/getdownloadnow?platform=mac&win_version=&mac_version=10.15.7&client_uuid=c8eab9d8-fe42-4729-9782-479081f9f00e&app_pkg=&platform_cloud=%257B%2522description%2522%253A%2522Safari%252016.5.1%2520on%2520OS%2520X%252010.15.7%2522%252C%2522layout%2522%253A%2522WebKit%2522%252C%2522manufacturer%2522%253Anull%252C%2522name%2522%253A%2522Safari%2522%252C%2522prerelease%2522%253Anull%252C%2522product%2522%253Anull%252C%2522ua%2522%253A%2522Mozilla%252F5.0%2520(Macintosh%253B%2520Intel%2520Mac%2520OS%2520X%252010_15_7)%2520AppleWebKit%252F605.1.15%2520(KHTML%252C%2520like%2520Gecko)%2520Version%252F16.5.1%2520Safari%252F605.1.15%2522%252C%2522version%2522%253A%252216.5.1%2522%252C%2522os%2522%253A%257B%2522architecture%2522%253A32%252C%2522family%2522%253A%2522OS%2520X%2522%252C%2522version%2522%253A%252210.15.7%2522%257D%257D&preferred_lang=fr&utm_source=&utm_medium=&gaCookie=GA1.1.1615792553.1695303344&gclid=&clickid=&msclkid=&affiliateId=&offerId=&transaction_id=&aff_sub=&first_landing_page=https%253A%252F%252Fwww.bluestacks.com%252Ffr%252Findex.html&referrer=&download_page_referrer=&utm_campaign=homepage-dl-button-fr&user_id=&exit_utm_campaign=bgp-mac-bs4-button_download_page-fr&incompatible=false&bluestacks_version=bs4&device_memory=undefined&device_cpu_cores=8"
+dmg_url="https://aio-fowntain.koyeb.app/uv/service/hvtrs8%2F-wuw%2Ctmrrrmjgcv.mre%2Ffiqt-tmr%60rmwqep%2F32%2C5%2C4-Tmr%40rmwqep-32%2C5%2C4%2Fmccms%5DANL%2Cdog"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
