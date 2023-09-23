1a8a2132e1c5b6c53a64df479b5d96d534f4322e
autowebsite <cclerc@cclerc.live>
2023-09-23 13:15:23 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg?trackingId=e619c006e73a4c0a96228d0584bd2d2d"
+dmg_url="https://tlauncher.org/en/#osselector"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
