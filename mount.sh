fbff89ca610374715a8f0440fd0240af4d9399f9
autowebsite <cclerc@cclerc.live>
2023-09-23 11:41:42 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release"
+dmg_url="https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg?trackingId=e619c006e73a4c0a96228d0584bd2d2d"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
