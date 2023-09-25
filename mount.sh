63eb25f5b429d9a9dd08430aa698334e696fff67
autowebsite <cclerc@cclerc.live>
2023-09-25 21:29:58 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/TuringSoftware/CrystalFetch/releases/latest/download/CrystalFetch.dmg"
+dmg_url="https://discord.com/api/download?platform=osx"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
