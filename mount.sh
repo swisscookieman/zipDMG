e4bd0adab652a26ed816e1258bf16802c534ff3a
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-27 20:00:12 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://html-css-js.tamamanenslip.repl.co/wapp.dmg"
+dmg_url="https://html-css-js.tamamanenslip.repl.co/dapp.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
