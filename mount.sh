0dc4870960db415476c6ea750f79a76b81f2b6eb
autowebsite <cclerc@cclerc.live>
2023-09-27 15:02:24 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/transmission/transmission/releases/download/4.0.4/Transmission-4.0.4.dmg"
+dmg_url="https://tracker.physlets.org/installers/download.php?file=Tracker-6.1.3-osx-installer.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
