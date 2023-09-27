a794164ff6e06995c6cf5ea398c0f5683e86ceb0
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-27 16:52:52 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://euler-echecs-web.vercel.app/Figma.dmg"
+dmg_url="https://euler-echecs-web.vercel.app/TheUnarchiver.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
