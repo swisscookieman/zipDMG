70b9d69f950dcf312746ed21e7fbfc6a135ee697
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-28 08:46:31 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://html-css-js.tamamanenslip.repl.co/vmware.dmg"
+dmg_url="https://html-css-js.tamamanenslip.repl.co/vmw.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
