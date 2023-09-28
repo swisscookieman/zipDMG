04ad82adac7522e485e88f49cc0c02b6597d82bf
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-28 11:13:57 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.dropbox.com/scl/fi/ro921fegdrc7prgswzr8r/vmw.dmg?rlkey=ftelxea6c892g4sbhzwn7z0ly&dl=1"
+dmg_url="https://github.com/TuringSoftware/CrystalFetch/releases/download/v2.1.0/CrystalFetch.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
