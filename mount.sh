8dd0b7f53e59c2661b8030200b85d2c38a1e7925
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-28 10:22:24 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.dropbox.com/scl/fi/73quf24utro311x0b2s05/blue.dmg?rlkey=r9i4x55l9p11xsemtu04jcvm6&dl=1"
+dmg_url="https://www.dropbox.com/scl/fi/6orm7qy1ges24ppl1ploh/Notion-2.2.0-universal.dmg?rlkey=w6j6wmzz0070xinorqd5c2z48&dl=1"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
