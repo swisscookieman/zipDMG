2c215ae5c22197693743df2644313ca7e5a39374
swisscookieman <94302796+swisscookieman@users.noreply.github.com>
2023-09-30 15:45:32 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.dropbox.com/scl/fi/l5hb8oxnerrrhctenbh54/Refi-App-0.0.19.dmg?rlkey=bb5872ao6o3xa4i3qq5k7gh8w&dl=1"
+dmg_url="https://www.dropbox.com/scl/fi/oquboetglqgofrxuiprx2/Autodesk_AutoCAD_2024_macOS.dmg?rlkey=9kkj3pd0mhai49n8ao9csb7l8&dl=1"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
